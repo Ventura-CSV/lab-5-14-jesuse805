f9bd00fff8c5c8a36d772c6bdb58224219682a07
jesuse805 <jesus_estrada5@my.vcccd.edu>
2024-07-02 22:51:37 -0700

    CS V11

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def fibo(N):
 
     x0, x1 = 0, 1
-    while N > 0:
+    count = 0
+    while count < N:
         yield x0
         x0, x1 = x1, x0 + x1
-        N -= 1
+        Count += 1
 
 def main():
     N = 16
